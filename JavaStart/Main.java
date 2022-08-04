4fa31d3e0093176a5034d19b0828721906b6b5b0
Bearboy27 <appalahemanth413@gmail.com>
2022-08-04 23:48:43 +0530

    Arrays 2d

---
+++ JavaStart/Main.java
@@ -0,0 +1,45 @@
+package JavaStart.JavaStart;
+
+import java.util.Arrays;
+
+public class Main{
+    // Binary Search on sorted 2D array
+
+
+        static int[] findAns(int[][] arr, int target)
+        {
+            int row = 0;
+            int col = arr[row].length - 1;
+            while (row < arr.length && col >= 0) {
+                if (arr[row][col] == target) {
+                    return new int[] { row, col };
+                }
+
+                // Target lies in further row
+                if (arr[row][col] < target) {
+                    row++;
+                }
+                // Target lies in previous column
+                else {
+                    col--;
+                }
+            }
+            return new int[] { -1, -1 };
+        }
+
+        // Driver Code
+        public static void main(String[] args)
+        {
+
+            // Binary search in sorted matrix
+            int arr[][] = { { 10, 20, 30, 40 },
+                    { 15, 25, 35, 45 },
+                    { 27, 29, 37, 48 },
+                    { 32, 33, 39, 50}};
+            int[] ans = findAns(arr, 29);
+            System.out.println("Element found at index: "
+                    + Arrays.toString(ans));
+        }
+    }
+
+
